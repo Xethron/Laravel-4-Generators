568f1c4f809dad1c0cfe9698a9471debc193430a
Türker YILDIRIM <me@turkeryildirim.com>
2017-09-21 01:36:29 +0300

    Update ScaffoldGeneratorCommand.php

--- src/Way/Generators/Commands/ScaffoldGeneratorCommand.php
+++ src/Way/Generators/Commands/ScaffoldGeneratorCommand.php
@@ -3,7 +3,7 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
-use Config;
+use \Illuminate\Support\Facades\Config as Config;
 
 class ScaffoldGeneratorCommand extends ResourceGeneratorCommand {
 
